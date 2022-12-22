96a42ef51833a8f8d06c205bb9728da27d8580be
KTaylor264 <k.taylor264@yahoo.com>
2022-12-21 23:16:43 -0500

    HM Damage Calc Project

---
+++ HMCalc/src/main/java/com/kendelltaylor/hmcalc/HmCalcApplication.java
@@ -0,0 +1,13 @@
+package com.kendelltaylor.hmcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmCalcApplication.class, args);
+	}
+
+}
